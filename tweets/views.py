48dc2a2118d5265ae89938967d1f1978988903b8
amanraj209 <amanraj20996@gmail.com>
2017-11-28 15:06:31 +0530

    Dynamic routing and CBV

--- tweets/views.py
+++ tweets/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from .models import Tweet
 
 
-def tweet_detail_view(request, id=1):
-    obj = Tweet.objects.get(id=id)
-    context = {
-        'object': obj
-    }
-    return render(request, 'tweets/detail_view.html', context)
-
-
-def tweet_list_view(request):
+class TweetDetailView(DetailView):
     queryset = Tweet.objects.all()
-    context = {
-        'object_list': queryset
-    }
-    return render(request, 'tweets/list_view.html', context)
+
+    # def get_object(self):
+    #     pk = self.kwargs.get('pk')
+    #     obj = get_object_or_404(Tweet, pk=pk)
+    #     return obj
+
+
+class TweetListView(ListView):
+    queryset = Tweet.objects.all()
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(TweetListView, self).get_context_data(*args, **kwargs)
+        return context
+
