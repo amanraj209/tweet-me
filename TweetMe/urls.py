13f64f1d085f3a2e6cecf6adffbb1c70bab8f1d2
amanraj209 <amanraj20996@gmail.com>
2017-11-30 22:19:19 +0530

    Route changes

--- TweetMe/urls.py
+++ TweetMe/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from tweets.views import TweetListView
 from .views import home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home, name='home'),
+    url(r'^$', TweetListView.as_view(), name='home'),
     url(r'^tweet/', include('tweets.urls',  namespace='tweet'))
 ]
